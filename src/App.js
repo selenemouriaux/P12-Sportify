6244ea15b6f13626fbc7d995e567cdf7e855e7c4
Selene Mouriaux <magicneuron@gmail.com>
2023-05-24 08:15:00 +0200

    added some data and a D3 visualisation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
+import Recharts from "./components/Recharts";
+import D3 from "./components/D3";
 
 function App() {
   return (
-        <p>
-          Ma page web Sportify
-        </p>
-
-  );
+    <>
+      <p>
+        Ma page web Sportify
+      </p>
+      <D3 />
+      <hr/>
+      <Recharts />
+    </>
+  )
+    ;
 }
 
 export default App;
---
+++ src/components/Recharts/index.jsx
@@ -0,0 +1,16 @@
+import './style.css'
+// import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+// import {default as data} from '../../data/user1AverageSession.json'
+
+
+const Recharts = () => {
+  return (
+    <>
+        <h1>Recharts graph :</h1>
+        <br/>
+
+    </>
+  )
+}
+
+export default Recharts;
